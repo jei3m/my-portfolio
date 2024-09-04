4255e06de62517f73228f40f9bdd00b7f48c1560
Justin Miguel Reyes <virozzegt@gmail.com>
2024-09-04 17:21:11 +0800

    "Update Email page layout and styling"

--- app/email/page.tsx
+++ app/email/page.tsx
@@ -64,8 +64,8 @@ const Email = () => {
     <div className="dark:bg-black dark: bg-grid-white/[0.1]">
       <Header/>
       <Script src="https://cdn.tailwindcss.com?plugins=forms,container-queries" strategy="beforeInteractive" />
-      <div className="flex flex-col min-h-screen items-center justify-center text-gray-900 w-full dark:bg-black dark:bg-grid-white/[0.1]" style={{ fontFamily: '"Space Grotesk", "Noto Sans", sans-serif' }}>
-        <div className="shadow-lg rounded-lg p-8 w-[90%] h-[90vh] max-w-3xl mx-auto overflow-y-auto">
+      <div className="overflow-x-hidden overflow-hidden w-full dark:bg-black dark: bg-grid-white/[0.1] relative flex items-center justify-center min-h-screen" style={{ fontFamily: '"Space Grotesk", "Noto Sans", sans-serif' }}>
+        <div className="p-8 w-[90%] h-[90vh] max-w-3xl mx-auto overflow-y-auto">
           <div className="flex items-center justify-between mb-6">
             <h2 className="text-white text-4xl font-bold" style={{ fontFamily: "DM Serif Display, serif" }}>Email me</h2>
             <div
