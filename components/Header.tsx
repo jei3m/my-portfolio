ae2f8ec5ab225ccb491716f03e1e371f8733ea3b
Justin Miguel Reyes <virozzegt@gmail.com>
2024-09-04 17:00:59 +0800

    "Updated Header and FloatingNav components: added new icons, removed unnecessary styles, and refactored FloatingNav logic."

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,44 +1,30 @@
 "use client";
 import React from "react";
 import { FloatingNav } from "../components/ui/floating-navbar";
-import { IconHome, IconMail } from "@tabler/icons-react";
+import { IconHome, IconMessage, IconUser, IconFolder, IconMail } from "@tabler/icons-react";
 
 export default function Header() {
   const navItems = [
     {
       name: "Home",
       link: "/",
-      icon: (
-        <IconHome 
-          className="h-4 w-4 text-neutral-500 dark:text-white bg-black dark:bg-black" 
-          style={{ backgroundColor: 'black' }} 
-        />
-      ),
+      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     {
       name: "Email me",
       link: "/email",
-      icon: (
-        <IconMail 
-          className="h-4 w-4 text-neutral-500 dark:text-white bg-black dark:bg-black" 
-          style={{ backgroundColor: 'black' }} 
-        />
-      ),
+      icon: <IconMail className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     // {
     //   name: "Resume",
     //   link: "/contact",
     //   icon: (
-    //     <IconMessage 
-    //       className="h-4 w-4 text-neutral-500 dark:text-white bg-black dark:bg-black" 
-    //       style={{ backgroundColor: 'black' }} 
-    //     />
+    //     <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
     //   ),
     // },
   ];
-  
   return (
-    <div className="relative w-full">
+    <div className="relative  w-full">
       <FloatingNav navItems={navItems} />
       {/* <DummyContent /> */}
     </div>
--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -4,7 +4,11 @@ import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
-import { useScroll, useMotionValueEvent } from "framer-motion";
+
+import {
+  useScroll,
+  useMotionValueEvent,
+} from "framer-motion";
 
 export const FloatingNav = ({
   navItems,
@@ -17,12 +21,19 @@ export const FloatingNav = ({
   }[];
   className?: string;
 }) => {
+
+  // Always set visible to true
+  // const visible = true;
+
   const [visible, setVisible] = useState(false);
+
   const { scrollYProgress } = useScroll();
 
   useMotionValueEvent(scrollYProgress, "change", (current) => {
+    // Check if current is not undefined and is a number
     if (typeof current === "number") {
-      let direction = current - scrollYProgress.getPrevious()!;
+      let direction = current! - scrollYProgress.getPrevious()!;
+ 
       if (scrollYProgress.get() < 0.01) {
         setVisible(true);
       } else {
@@ -50,7 +61,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full bg-black dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2 items-center justify-center space-x-4",
+          "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4",
           className
         )}
       >
@@ -68,8 +79,7 @@ export const FloatingNav = ({
         ))}
         <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
           <a href="#contacts">
-            <span> Résumé</span>
-          </a>
+          <span> Résumé</span></a>
           <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
         </button>
       </motion.div>
