d1a13737e31578582fe6e89cd67a07506318fe49
jei3m <virozzegt@gmail.com>
2024-10-30 21:53:29 +0800

    "Updated padding from p-6 to p-4 in CV page container"

--- app/opencv/page.tsx
+++ app/opencv/page.tsx
@@ -16,7 +16,7 @@ const CVPage = () => {
 
   return (
     <div className="flex flex-col overflow-x-hidden overflow-y-auto min-h-screen bg-black bg-grid-white/[0.2] relative flex items-center justify-center">
-    <div className="p-6 flex flex-col overflow-x-hidden overflow-hidden w-full h-full relative flex items-center justify-center">
+    <div className="p-4 flex flex-col overflow-x-hidden overflow-hidden w-full h-full relative flex items-center justify-center">
       <Header />
       <Head>
         <title>My CV</title>
